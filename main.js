12ab63ae0fa390c95607e4fbd8b013fe9a5a57a5
2TruesWebDev <121456515+2TruesWebDev@users.noreply.github.com>
2023-01-25 16:57:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const hamburgerToggle = document.getElementById('hamburger-toggle');
+const link = document.getElementById('link');
+
+
+
+hamburgerToggle.addEventListener('click', () => {
+    link.classList.toggle('active')
+})
+
+
+
+
+
